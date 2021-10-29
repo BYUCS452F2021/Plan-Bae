d2cb2d406872c82ed8685ce2260ecc677b339790
Tylan <tylan07@gmail.com>
2021-10-29 07:21:11 -0600

    Created POST endpoints, made necessary modifications to models and schema classes

--- backend/app/models.py
+++ backend/app/models.py
@@ -6,19 +6,21 @@ class Activity(Base):
     activity_id = Column(Integer, unique = True, primary_key = True)
     name = Column(String)
     tags = Column(String)
-    active = Column(Integer)
-    description = Column(String)
+    active_level = Column(Integer)
+    activity_description = Column(String)
     url = Column(String)
-    min_cost = Column(Integer)
+    cost = Column(Integer)
     duration = Column(Integer)
     latitude = Column(String)
     longitude = Column(String)
 
+
 class Date(Base):
     __tablename__ = "Date"
-    date_id = Column(Integer, unique=True,primary_key = True)
+    date_id = Column(Integer, unique=True, primary_key = True)
     user_id = Column(Integer, unique = True)
     time = Column(DateTime)
+
 
 class User(Base):
     __tablename__ = "User"
@@ -26,11 +28,9 @@ class User(Base):
     username = Column(String)
     password = Column(String)
 
+
 class DateActivity(Base):
     __tablename__ = "DateActivity"
     dateactivity_id = Column(Integer, unique = True, primary_key = True)
     date_id = Column(Integer)
     activity_id = Column(Integer)
-
-
-
