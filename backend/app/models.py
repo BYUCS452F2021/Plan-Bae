2fe6e7d3c9518bfdbb16062598be24d2f80d4265
TraceHale24 <trace.hale24@gmail.com>
2021-10-28 23:03:08 -0600

    Fixed

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,4 @@
 from .database import Base
-from pydantic import BaseModel
 
 class Activity(Base):
     __tablename__ = "Activity"
@@ -33,32 +32,4 @@ class DateActivity(Base):
     activity_id = Column(Integer)
 
 
-class Activity(Base):
-    activity_id: int
-    name: str
-    tags: str
-    active: str
-    description: str
-    url: str
-    min_cost: int
-    duration: int
-    latitude: float
-    longitude: float
 
-
-class Date(Base):
-    date_id: int 
-    user_id: int 
-    time: DateTime
-
-class User(Base):
-    user_id: int
-    username: str 
-    password: str
-
-
-
-class DateActivity(Base):
-    dateactivity_id: int
-    date_id: int
-    activity_id: int
